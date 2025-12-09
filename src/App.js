0a68aa85b36f498df25c875298431a094ed3a378
Gokulshivakumar <gokulshiva1268@gmail>
2025-12-09 16:57:22 +0530

    Updated App.js and fixed components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import Forheader from './components/header';
 import Banner from './components/banner';
-import Sectiontransfer from './components/sectiontwo';
 import Sectionlayerthree from './components/sectiontwo';
 
 
