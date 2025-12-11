c8f952041f2afd126e7cec38b4d3f0e51274a72d
Gokulshivakumar <gokulshiva1268@gmail>
2025-12-11 14:23:57 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,20 @@ import './App.css';
 import Aboutus from './Websitegv/Pages/Aboutus.js';
 import Homepage from './Websitegv/Pages/Home.jsx';
 import Forheader from './Websitegv/components/header.js';
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
-    <HashRouter>
+    <Router>
     
       <Forheader />
-
-
-      {/* Page content changes based on URL */}
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<Aboutus />} />
         {/* Add more pages like Services, Contact */}
       </Routes>
-    </HashRouter>
+    </Router>
   );
 }
 
