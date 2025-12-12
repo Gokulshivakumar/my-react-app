924733f63245d84a713d08664b8d6aa264f72ca0
Gokulshivakumar <gokulshiva1268@gmail>
2025-12-12 12:20:50 +0530

    update

--- src/Websitegv/Pages/Aboutus.js
+++ src/Websitegv/Pages/Aboutus.js
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 function Aboutus(){
+    const [color,setcolor]=useState("green");
+    const [count,setcount]=useState(0);
+
+    const setvalue= ()=>{
+      setcolor(color=="red"? "blue": "yellow");
+    }
+
+  
     return (
       <div className="page-container" >
     <h1> About page </h1>
-    <p>Welcome to our About page GV.</p>
-
-   <button>Click me</button>
+    <p>My inisutial color was {color}</p>
+   <button onClick={setvalue}>Click me</button>
+   <h1>Working on counter mode</h1>
+   <h1>default counter value {count}</h1>
+   <button  onClick={()=>setcount(count+1)}>Increase</button><br></br>
+   <button onClick={()=>setcount(count-1)}>Decrease</button><br></br>
+   <button onClick={()=>setcount(0)}>Reset</button>
 
     </div>);
 };
